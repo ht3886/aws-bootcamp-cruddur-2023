ff899df9aaca4762f9077ce04f9af83bcded46c7
Harshit Trivedi <htrivedi3886@gmail.com>
2023-03-24 23:16:52 +0000

    update cognito user_id for users

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,14 +1,14 @@
 -- this file was manually created
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('Mr. Trivedi', 'ht3886@gmail.com', 'ht3886' ,'MOCK'),
-  ('Andrew Brown', 'andrew@exampro.co', 'andrewbrown' ,'MOCK'),
-  ('Andrew Bayko', 'bayko@exampro.co', 'bayko' ,'MOCK');
+  ('Mr. Trivedi', 'ht3886@gmail.com', 'ht3886' ,'c93681f2-cbd4-4396-b766-26918f34ed62'),
+  ('Andrew Brown', 'andrew@exampro.co', 'andrewbrown' ,'f73f4a05-a59e-468a-8a29-a1c39e7a1111'),
+  ('Andrew Bayko', 'bayko@exampro.co', 'bayko' ,'f73f4a05-a59e-468a-8a29-a1c39e7a2222');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
+    (SELECT uuid from public.users WHERE users.handle = 'ht3886' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
   )
