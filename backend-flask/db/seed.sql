ae2603ebbd36eab7318f48095469f38eac138ddc
Harshit Trivedi <htrivedi3886@gmail.com>
2023-03-25 02:38:22 +0000

    implemented DynamoDB functionality and access pattern A-D

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -2,8 +2,8 @@
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
   ('Mr. Trivedi', 'ht3886@gmail.com', 'ht3886' ,'c93681f2-cbd4-4396-b766-26918f34ed62'),
-  ('Andrew Brown', 'andrew@exampro.co', 'andrewbrown' ,'f73f4a05-a59e-468a-8a29-a1c39e7a1111'),
-  ('Andrew Bayko', 'bayko@exampro.co', 'bayko' ,'f73f4a05-a59e-468a-8a29-a1c39e7a2222');
+  ('Andrew Bayko', 'bayko@exampro.co', 'bayko' ,'f73f4a05-a59e-468a-8a29-a1c39e7a2222'),
+  ('Londo Mollari','lmollari@centari.com' ,'londo' ,'f73f4a05-a59e-468a-8a29-a1c39e7a5555');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
